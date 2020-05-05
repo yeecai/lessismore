f5d22899757b3c4ce834ae5adee557a1235d1c75
ye cai <yushunchn@gmail.com>
2020-05-05 15:36:10 +0800

    Updates

--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { authReducer, isModalShowReducer } from "./auth_reducer.js";
-
-export default combineReducers({
-    auth: authReducer,
-    isLoginModalShow: isModalShowReducer
-});
-
